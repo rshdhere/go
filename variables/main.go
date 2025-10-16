46c05f368b10eb1502f0cd672484c138284392ee
raashed <mohammedraashed10@gmail.com>
2025-10-16 13:57:18 +0530

    learn: variables

---
+++ variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "mbappe"
+	fmt.Printf("hello my name is %s\n", name)
+
+	myName := "ronaldo"
+	myInt := 10
+	myFloat := 23.455
+
+	fmt.Printf("hello my name is %s my int is %d and my float is %f\n", myName, myInt, myFloat)
+}
