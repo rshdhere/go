4c215b9d6de241f3844ea09aa19c08b839027e81
raashed <mohammedraashed10@gmail.com>
2025-10-17 11:35:18 +0530

    learn: importing

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"basics/imports"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("hello world")
+	newTicket := imports.Ticket{
+		ID:    123,
+		Event: "yada-yada",
+	}
+
+	newTicket.PrintEvent()
+
+	fmt.Println(newTicket)
 }
